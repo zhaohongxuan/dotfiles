06751091555b7e280a7f1f01fa288b3efa797a61
zhaohongxuan <hxzhenu@gmail.com>
2022-08-04 18:50:34 +0800

    update wezterm config

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -23,10 +23,17 @@ return {
   -- https://wezfurlong.org/wezterm/config/font-shaping.html
   harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' },
 
-  -- Cursor style
-  default_cursor_style = 'BlinkingBar',
-
-  -- Enable CSI u mode
-  -- https://wezfurlong.org/wezterm/config/lua/config/enable_csi_u_key_encoding.html
-  enable_csi_u_key_encoding = true
+  leader = { key = 'a', mods = 'CMD', timeout_milliseconds = 1000 },
+  keys = {
+    {
+      key = '|',
+      mods = 'LEADER|SHIFT',
+      action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+    },
+    {
+      key = '-',
+      mods = 'LEADER',
+      action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
+    },
+  },
 }
