bf7ba90b5038febe745336edae6c6a1b7d6847b5
zhaohongxuan <hxzhenu@gmail.com>
2023-07-24 23:47:19 +0800

    update nvim config to lua

---
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,22 @@
+require("mason").setup({
+  ui = {
+      icons = {
+          package_installed = "✓",
+          package_pending = "➜",
+          package_uninstalled = "✗"
+      }
+  }
+})
+
+require("mason-lspconfig").setup({
+  -- 确保安装，根据需要填写
+  ensure_installed = {
+    "lua_ls",
+  },
+})
+
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+require("lspconfig").lua_ls.setup {
+  capabilities = capabilities,
+}
---
+++ dot_config/nvim/lua/plugins/plugins-setup.lua
@@ -0,0 +1,57 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+
+local plugins = {
+  "folke/tokyonight.nvim", -- 主题
+  "nvim-lualine/lualine.nvim",  -- 状态栏
+  "nvim-tree/nvim-tree.lua",  -- 文档树
+  "nvim-tree/nvim-web-devicons", -- 文档树图标
+
+  "christoomey/vim-tmux-navigator", -- 用ctl-hjkl来定位窗口
+  "nvim-treesitter/nvim-treesitter", -- 语法高亮
+  "p00f/nvim-ts-rainbow", -- 配合treesitter，不同括号颜色区分
+  {
+    'goolord/alpha-nvim',
+    lazy = true
+  },
+
+  {
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim", -- 这个相当于mason.nvim和lspconfig的桥梁
+    "neovim/nvim-lspconfig"
+  },
+
+      -- 自动补全
+  "hrsh7th/nvim-cmp",
+  "hrsh7th/cmp-nvim-lsp",
+  "L3MON4D3/LuaSnip", -- snippets引擎，不装这个自动补全会出问题
+  "saadparwaiz1/cmp_luasnip",
+  "rafamadriz/friendly-snippets",
+  "hrsh7th/cmp-path", -- 文件路径
+
+  "numToStr/Comment.nvim", -- gcc和gc注释
+  "windwp/nvim-autopairs", -- 自动补全括号
+
+  "akinsho/bufferline.nvim", -- buffer分割线
+  "lewis6991/gitsigns.nvim", -- 左则git提示
+
+  {
+    'nvim-telescope/telescope.nvim', tag = '0.1.1', -- 文件检索
+    dependencies = { {'nvim-lua/plenary.nvim'} } -- requires要改为dependencies
+  },
+
+}
+local opts = {} -- 注意要定义这个变量
+
+require("lazy").setup(plugins, opts)
